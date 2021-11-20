60ce0e51e167249e85b209ddbc98093c2e84d1e2
senyabird <senyabird@yandex.ru>
2021-11-20 20:30:56 +0300

    boot-hibernate-h2 homework completed

--- homework5/lightVersion/src/main/resources/import.sql
+++
@@ -1,3 +0,0 @@
-DROP TABLE products IF EXISTS;
-CREATE TABLE IF NOT EXISTS products (id bigserial, title VARCHAR(255), cost int, PRIMARY KEY (id));
-INSERT INTO products (title, cost) VALUES ('milk', 105), ('bread', 30), ('jam', 100);
