e7843227b83186f5eda738b57497a713d77039b1
senyabird <senyabird@yandex.ru>
2021-11-20 20:23:25 +0300

    homework5 completed

---
+++ homework5/fullVersion/src/test/java/ru/geekbrains/hibernate/h2/HibernateH2AppApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.geekbrains.hibernate.h2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HibernateH2AppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
